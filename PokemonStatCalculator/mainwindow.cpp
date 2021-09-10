625daefc648be511956540b35f0412bfc5a1b649
Patrick Henegan <phenegan03@gmail.com>
2021-09-10 13:10:13 -0400

    connected the 'Calculate' button to the backend functions depending on the mode selected.

--- PokemonStatCalculator/mainwindow.cpp
+++ PokemonStatCalculator/mainwindow.cpp
@@ -20,18 +20,30 @@ void MainWindow::on_actionCalculateEV_triggered()
 {
     m_currentOperation = CalcMode::EV;
     setWindowTitle("Pokemon EV Range Calculation");
+    ui->label1->setText("Total Stat:");
+    ui->label2->setText("Base Stat:");
+    ui->label3->setText("Level");
+    ui->label4->setText("IV:");
 }
 
 void MainWindow::on_actionCalculateIV_triggered()
 {
     m_currentOperation = CalcMode::IV;
     setWindowTitle("Pokemon IV Range Calculation");
+    ui->label1->setText("Total Stat:");
+    ui->label2->setText("Base Stat:");
+    ui->label3->setText("Level");
+    ui->label4->setText("EV:");
 }
 
 void MainWindow::on_actionCalculateBaseStat_triggered()
 {
     m_currentOperation = CalcMode::BASE_STAT;
     setWindowTitle("Pokemon Base Stat Range Calculation");
+    ui->label1->setText("Total Stat:");
+    ui->label2->setText("Level:");
+    ui->label3->setText("EV");
+    ui->label4->setText("IV:");
 }
 
 void MainWindow::on_actionCalculateTotalStat_triggered()
@@ -57,8 +69,27 @@ void MainWindow::on_natureMods_currentTextChanged(const QString &arg1)
 
 void MainWindow::on_pushCalculate_clicked()
 {
+    int value1 = ui->input1->value();
+    int value2 = ui->input2->value();
+    int value3 = ui->input3->value();
+    int value4 = ui->input4->value();
 
+    std::vector<int> range;
+    switch(m_currentOperation) {
+        case CalcMode::EV:
+            range = findEV(value1, value2, value3, value4, m_isHP, m_natureMod);
+        break;
+        case CalcMode::IV:
+            range = findIV(value1, value2, value3, value4, m_isHP, m_natureMod);
+        break;
+        case CalcMode::BASE_STAT:
+            range = findBaseStat(value1, value2, value3, value4, m_isHP, m_natureMod);
+        break;
+        case CalcMode::LEVEL:
+            //TODO: write backend for this
+        break;
+        case CalcMode::STAT:
+            //TODO: write backend for this
+        break;
+    }
 }
-
-
-
